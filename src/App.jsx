ff5d71754f8af90abea86053fc5ced9217d4888a
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-05-10 20:21:53 +0700

    p

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
+import LoginPage from "./pages/LoginPage";
+
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <HomePage />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
