d0020f9f63521d33b67e413de6b003cd74af8d7b
Rasyad Jabbar Poncosewoyo <rasyadponcosewoyo@gmail.com>
2024-05-15 21:43:25 +0700

    feature: message item
    add: message's date in UTC format

--- src/pages/home/MessageItem.jsx
+++ src/pages/home/MessageItem.jsx
@@ -2,11 +2,17 @@ import {Card} from "react-bootstrap";
 import PropTypes from "prop-types";
 
 export default function MessageItem({message}) {
+    const messageDate = () => {
+        const date = new Date(message.createdAt);
+        return date.toUTCString();
+    }
+
     return (
         <>
             <Card className={"mb-3"} body>
                 <p>{message.createdBy}</p>
                 <h4>{message.body}</h4>
+                <p>{messageDate()}</p>
             </Card>
         </>
     );
