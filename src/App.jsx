13ed0abbefc1a2966bdee73677e7198db2a6e561
FauziViko <152051797+fauziviko@users.noreply.github.com>
2024-05-16 20:52:13 +0700

    login google frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage  from "./pages/HomePage";
-import Register from "./pages/Register";
-import LoginPage from "./pages/LoginPage";
+import Register from "./pages/Register"
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import store from "./redux/store";
 
 const router = createBrowserRouter([
     {
@@ -14,14 +18,17 @@ const router = createBrowserRouter([
       path: "/register",
       element: <Register />,
   },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
 ]);
 
 function App() {
-    return <RouterProvider router={router} />;
+    return (
+    <Provider store={store}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <RouterProvider router={router} />
+        <ToastContainer theme="colored" />
+        </GoogleOAuthProvider>
+    </Provider>
+    );
 }
 
 export default App;
---
+++ src/redux/actions/register.js
@@ -0,0 +1,77 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import { setToken, setUser } from "../reducers/auth";
+
+export const register =
+    (navigate, username, email, password, name, photo, setIsLoading) =>
+    async (dispatch) => {
+        // make loading
+        setIsLoading(true);
+
+        let data = new FormData();
+        data.append("username", username);
+        data.append("email", email);
+        data.append("password", password);
+        data.append("name", name);
+        if (photo) {
+            data.append("photo", photo);
+        }
+
+        let config = {
+            method: "post",
+            url: `${import.meta.env.VITE_BACKEND_API}/api/register`,
+            data: data,
+        };
+
+        try {
+            const response = await axios.request(config);
+            console.log(response)
+            // get and save the token to local storage
+            const { data } = response.data;
+            const { token } = data;
+            localStorage.setItem("token", token);
+
+            // redirect to home
+            navigate("/");
+        } catch (error) {
+            toast.error(error?.response?.data?.message);
+
+        }
+
+        setIsLoading(false);
+    };
+
+export const loginWithGoogle = (navigate, accessToken) => async (dispatch) => {
+        let data = JSON.stringify({
+          access_token: accessToken,
+        });
+      
+        let config = {
+          method: "post",
+          url: `${import.meta.env.VITE_BACKEND_API}/api/auth/google-login`,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          data: data,
+        };
+      
+        try {
+          const response = await axios.request(config);
+          console.log(response);
+      
+          // get and save the token to local storage
+          const { data } = response.data;
+          const { token, user } = data;
+      
+          // Change the token value in the reducer
+          dispatch(setToken(token));
+          dispatch(setUser(user));
+      
+          // redirect to home
+          navigate("/"); // it will be not consistent, so alternative we use window until we used the state management
+        } catch (error) {
+          toast.error(error?.response?.data?.message);
+      
+        }
+      };
+      
